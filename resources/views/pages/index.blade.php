bb83622665144654746ee5ac8cb2b81459d27bc5
sueteng910 <sueteng910@gmail.com>
2018-10-08 14:50:19 +0800

    login fixed

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -7,7 +7,7 @@
             <div class="container">
               <h1 class="display-3">Laravel</h1>
               <p>This is a template for a simple marketing or informational website.</p>
-              <p><a class="btn btn-primary btn-lg" href="#" role="button">Login &raquo;</a> <a class="btn btn-primary btn-lg" href="#" role="button">Register &raquo;</a></p>
+              <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a> <a class="btn btn-primary btn-lg" href="/register" role="button">Register &raquo;</a></p>
             </div>
           </div>
     @endsection
