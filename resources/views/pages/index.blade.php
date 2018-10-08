d79c1d694d2bc96af9018d9e4f4695d94e302fb3
sueteng910 <sueteng910@gmail.com>
2018-10-08 14:27:44 +0800

    initial upload

---
+++ resources/views/pages/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+    @section('content')
+        
+    
+    <div class="jumbotron text-center">
+            <div class="container">
+              <h1 class="display-3">Laravel</h1>
+              <p>This is a template for a simple marketing or informational website.</p>
+              <p><a class="btn btn-primary btn-lg" href="#" role="button">Login &raquo;</a> <a class="btn btn-primary btn-lg" href="#" role="button">Register &raquo;</a></p>
+            </div>
+          </div>
+    @endsection
+
