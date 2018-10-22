9e2aca06641925d5d82dc0bc428d5519aacb90d8
tqk-gh <taheralkamel@gmail.com>
2018-10-22 19:42:35 +0800

    removing gender from RegisterController to fix conflict when merging

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -52,7 +52,6 @@ class RegisterController extends Controller
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users|email_domain:' . $data['email'] . '',
             'password' => 'required|string|min:6|confirmed',
-            'gender'=>'required|string'
         ]);
     }
 
@@ -68,7 +67,6 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'gender' => $data['gender'],
 
         ]);
     }
