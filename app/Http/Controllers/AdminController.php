35a4ede4ab72b3f8d507d737244146064e335347
Jhaaa11 <ahmadazharmustapha@gmail.com>
2018-11-18 22:18:27 +0800

    Update app/Http/Controllers/AdminController.php

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -5,14 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Admin;
 use DB;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
 
 class AdminController extends Controller
 {
         public function approve(){
+            $swap = DB::select('select * from swapping_request');
 
-            return view('admin.approval.approve');
+            return view('admin.approval.approve',['swap'=>$swap]);
     }
-
 
   
 
