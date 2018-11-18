4576cf6aca29c9da69b8ac8fb5f6ab6ec20be5b7
Jhaaa11 <ahmadazharmustapha@gmail.com>
2018-11-18 22:18:40 +0800

    Update resources/views/admin/approval/approve.blade.php

--- resources/views/admin/approval/approve.blade.php
+++ resources/views/admin/approval/approve.blade.php
@@ -5,35 +5,24 @@
     <div class="row col-md-6 col-md-offset-2 custyle">
     <table class="table table-striped custab">
     <thead>
-        <tr>
-            <th>ID</th>
-            <th>Product ID</th>
-            <th>Buyer ID</th>
-            <th>Seller ID</th>
-            <th class="text-center">Action</th>
-        </tr>
-    </thead>
             <tr>
-                <td>1</td>
-                <td>P1</td>
-                <td>News Cate</td>
-                <td>Johnny</td>
-                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-ok"></span> Approve</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Reject</a></td>
+                <td>ID</td>
+                <td>Product ID</td>
+                <td>Buyer ID</td>
+                <td>Seller ID</td>
+                <td>Status</td>
+                <td>Action</td>
             </tr>
+        @foreach($swap as $key => $data)
             <tr>
-                <td>2</td>
-                <td>P2</td>
-                <td>Main Products</td>
-                <td>Johnny</td>
-                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-ok"></span> Approve</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Reject</a></td>
+                <th>{{$data->id}}</th>
+                <th>{{$data->product_id}}</th>
+                <th>{{$data->buyer_id}}</th>
+                <th>{{$data-seller_id}}</th>
+                <th>{{$data-status}}</th>
+                <th class="text-center">Action</th>
             </tr>
-            <tr>
-                <td>3</td>
-                <td>P3</td>
-                <td>Parent Blogs</td>
-                <td>Johnny</td>
-                <td class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-ok"></span> Approve</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Reject</a></td>
-            </tr>
+        @endforeach
     </table>
     </div>
 </div>
