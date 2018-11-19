71f29f8d531ce44b733cfb899099806934e6c753
Jhaaa11 <ahmadazharmustapha@gmail.com>
2018-11-19 16:09:14 +0800

    Update approve.blade.php

--- resources/views/admin/approval/approve.blade.php
+++ resources/views/admin/approval/approve.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container">
-    <div class="row col-md-6 col-md-offset-2 custyle">
+    <div class="row col-md-10 col-md-offset-1 custyle">
     <table class="table table-striped custab">
     <thead>
             <tr>
@@ -11,7 +11,7 @@
                 <td>Buyer ID</td>
                 <td>Seller ID</td>
                 <td>Status</td>
-                <td>Action</td>
+                <td class="text-center">Action</td>
             </tr>
         @foreach($swap as $key => $data)
             <tr>
@@ -20,7 +20,7 @@
                 <th>{{$data->buyer_id}}</th>
                 <th>{{$data->seller_id}}</th>
                 <th>{{$data->status}}</th>
-                <th class="text-center">Action</th>
+                <th class="text-center"><a class='btn btn-info btn-xs' href="#"><span class="glyphicon glyphicon-ok"></span> Approve</a> <a href="#" class="btn btn-danger btn-xs"><span class="glyphicon glyphicon-remove"></span> Reject</a></th>
             </tr>
         @endforeach
     </table>
