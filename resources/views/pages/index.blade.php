a8d06b38533f93050be8cb1ad09bfd875b290e3b
Maddy <a160897@siswa.ukm.edu.my>
2018-11-27 00:38:31 +0800

    fixing conflict in Maddy's branch
    fixing conflict in Maddy's branch

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 
 @section('content')
-
+    
+          <link rel="stylesheet" type="text/css" href="{{url('assets/css/bootstrap.min.css')}}"/> 
+          <script src="{{url('assets/js/components/bootstrap.js')}}"></script>
 <div class="container">
 
         <div class="row">
@@ -37,6 +39,8 @@
         <html>
         <head>
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+        </head>
         <style>
         * {box-sizing: border-box;}
         body {font-family: Verdana, sans-serif;}
@@ -112,11 +116,91 @@
         @media only screen and (max-width: 300px) {
           .text {font-size: 11px}
         }
+
+    .slider-item{
+    border: 1px solid #E1E1E1;
+    border-radius: 5px;
+    background: #FFF;
+}
+.slider-item .slider-image img{
+    margin: 0;
+    width: 100%;
+}
+.slider-item .slider-main-detail{
+    padding: 10px;
+    border-radius: 0 0 5px 5px;
+}
+.slider-item:hover .slider-main-detail{
+    background-color: #dbeeee !important;
+}
+.slider-item .price{
+    float: left;
+    margin-top: 5px;
+}
+.slider-item .price h5{
+    line-height: 20px;
+    margin: 0;
+}
+.detail-price{
+    color: #219FD1;
+}
+.slider-item .slider-main-detail .rating{
+    color: #777;
+}
+.slider-item .rating{
+    float: left;
+    font-size: 17px;
+    text-align: right;
+    line-height: 52px;
+    margin-bottom: 10px;
+    height: 52px;
+}
+.slider-item .btn-add{
+    width: 50%;
+    float: left;
+    border-right: 1px solid #E1E1E1;
+}
+.slider-item .btn-details{
+    width: 50%;
+    float: left;
+}
+.controls{
+    margin-top: 20px;
+}
+.btn-info,.btn-info:visited,.btn-info:hover{
+  background-color: #21BBD8;
+  border-color: #21BBD8;
+}
+.btn-info{
+  margin-left:5px;
+}
+.slider-main-detail:hover{
+  background-color: #dbeeee !important;
+}
+.AddCart{
+  margin: 0px;
+  padding:5px;
+  border-radius:2px;
+  margin-right:10px;
+}
+.review {
+  margin-bottom: 5px;
+  padding-top:5px;
+}
+
+                .footer {
+                   margin-top:50px;
+                   position:fixed ;
+                   left: 0;
+                   bottom: 0;
+                   width: 100%;
+                   background-color: Black;
+                   color: white;
+                   text-align: center;
+                }
         </style>
-        </head>
         <body>
-        
-   
+    
    
         <div class="slideshow-container">
         
@@ -148,6 +232,98 @@
           <span class="dot"></span> 
         </div>
         
+        
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    
+
+    <div class="container" style="margin-bottom:50px;">
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <h3>New Products</h3>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-6 hidden-xs">
+                <div class="controls pull-right">
+                    <a class="left fa fa-chevron-left btn btn-info " href="#carousel-example" data-slide="prev"></a><a class="right fa fa-chevron-right btn btn-info" href="#carousel-example" data-slide="next"></a>
+                </div>
+            </div>
+        </div>
+        
+
+                <div class="row">
+
+                    <div id="carousel-example" class="carousel slide hidden-xs" data-ride="carousel" data-type="multi">
+                        <div class="carousel-inner">
+                            <div class="item active">
+                                <div class="row">
+                @forelse($products as $product)
+                                    <div class="col-md-3 col-sm-3 col-xs-12">
+                                        <div class="slider-item">
+                                            <div class="slider-image">
+                           
+
+                            <img class="img-responsive" src="{{ asset('images/' . $product->product_pic_1) }}" alt="{{$product->product_pic_1}}">
+                              </div>
+                              <div class="slider-main-detail">
+                                  <div class="slider-detail">
+                                      <div class="product-detail">
+                          <h5>{{$product->product_name}}</h5>
+                          <h5 class="detail-price">RM {{$product->product_price}}</h5>
+
+                        </div>
+                      </div>
+                      <div class="cart-section">
+                          <div class="row">
+                              <div class="col-md-6 col-sm-12 col-xs-6 review">
+                                  <i class="fa fa-star" aria-hidden="true"></i>
+                                  <i class="fa fa-star" aria-hidden="true"></i>
+                                  <i class="fa fa-star" aria-hidden="true"></i>
+                                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                                  <i class="fa fa-star-o" aria-hidden="true"></i>
+                              </div>
+                              <div class="col-md-6 col-sm-12 col-xs-6">
+                                  <a href="#" class="AddCart btn btn-info"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Add To Cart</a>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+
+                          @empty
+
+        <h3>No products</h3>
+
+        
+
+    @endforelse
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    
+                        
+                    
+
+
+
+
+
+                </div>
+                
+                
+                
+                <div class="footer">
+                  <p>UNIVERSITY TRADING SYSTEMS</p>
+                </div>
+
+
         <script>
         var slideIndex = 0;
         showSlides();
@@ -169,102 +345,19 @@
             setTimeout(showSlides, 2000); // Change image every 2 seconds
         }
         </script>
-        
-        </body>
-        </html> 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-                <?php
-                       class ProductLoopVariables{
-                       private $product_id = 'default Value';
-                         public function setVariables($product_id){
-                           $this->product_id = $product_id;
-                       }
-                          public function getProductId(){
-                            return $this->product_id;
-                          }
-                    }
-               ?> 
-                      
 
 
-                @forelse($products as $product)
-                <?php
-                     $product_id = $product->id;
-                     $ProductLoopVariables = new ProductLoopVariables();
-                      $ProductLoopVariables->setVariables($product_id);
-                  ?>
-                <div class="row">
-
-                    <div class="col-lg-5">
-                      <div class="card h-100">
-                           
-                            <div><a href="{{route('product',[$product->id], false) }}"><img class="card-img-top" src="{{ asset('images/' . $product->product_pic_1) }}" alt="{{$product->product_pic_1}}" style="max-width: 50%;
-                              max-height: 50%;"></a></div>
-                            <div class="card-body">
-                                <h4 class="card-title">
-                          <h4>Item Name:{{$product->product_name}}</h4>
-                          <h4>Price:RM {{$product->product_price}}</h4>
-                          <h4>ID {{$product_id}}</h4>
-                          <h4>Description:{{$product->product_description}}</h4>
-                        
-
-                        </div>
-                        <div class="card-footer">
-                          <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-                        </div>
-                      </div>
-                    </div>
-
-                          
-
-                          @empty
-
-        <h3>No products</h3>
-
-        
-
-    @endforelse
-
-
-
-
-
-                </div>
-               
-                <!DOCTYPE html>
-                <html>
-                <head>
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <style>
-                .footer {
-                   position:fixed ;
-                   left: 0;
-                   bottom: 0;
-                   width: 100%;
-                   background-color: Black;
-                   color: white;
-                   text-align: center;
-                }
-                </style>
-                </head>
-                <body>
+    <link rel="stylesheet" type="text/css" href="{{url('assets/css/bootstrap.min.css')}}"/>
+    <script src="{{url('assets/js/components/bootstrap.js')}}"></script>
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.theme.min.css"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/owl-carousel/1.3.3/owl.carousel.css"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
                 
-                
-                
-                <div class="footer">
-                  <p>UNIVERSITY TRADING SYSTEMS</p>
-                </div>
-                
-                <script>
-                  onclick="serieName=this.dataset.serieName;document.querySelector('#myModal input#product_id').value = serieName;return true;"
-              </script>
                 </body>
                 </html> 
                 
